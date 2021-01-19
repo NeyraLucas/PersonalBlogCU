0d4972b71582cdae15b6571b9b2b41367697e526
NeyraLucas <luiscrazypeople@gmail.com>
2021-01-19 16:46:33 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function VerP1(){
+    $('#v1').show();
+    $('#v2').hide();
+    $('#v3').hide();
+}
+
+function VerP2(){
+    $('#v1').hide();
+    $('#v2').show();
+    $('#v3').hide();
+}
+
+function VerP3(){
+    $('#v1').hide();
+    $('#v2').hide();
+    $('#v3').show();
+}
